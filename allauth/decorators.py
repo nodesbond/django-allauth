be6ce75932968020b367e02ed4fb0c6ce9cd1655
nodes.bond <150801441+nodesbond@users.noreply.github.com>
2024-02-21 12:46:17 +0600

    Update decorators.py
        Variable Naming: Changed the variable name resp to rate_limit_response for clarity. This distinguishes between the rate limit check response and the function's execution result.
    
        Function Documentation: Added a docstring to the rate_limit decorator to explain its purpose, parameters, and behavior. This enhances code readability and maintainability.
    
        Logic Clarification: Separated the logic for rate limit exceeded and function execution. This makes the code flow more understandable and explicit.

--- allauth/decorators.py
+++ allauth/decorators.py
@@ -1,16 +1,22 @@
 from functools import wraps
-
 from allauth.core import ratelimit
 
-
 def rate_limit(*, action, **rl_kwargs):
+    """
+    Decorator to apply rate limiting to a function.
+
+    :param action: The action to be evaluated for rate limiting.
+    :param rl_kwargs: Additional arguments for the rate limiting function.
+    """
     def decorator(function):
         @wraps(function)
         def wrap(request, *args, **kwargs):
-            resp = ratelimit.consume_or_429(request, action=action, **rl_kwargs)
-            if not resp:
-                resp = function(request, *args, **kwargs)
-            return resp
+            rate_limit_response = ratelimit.consume_or_429(request, action=action, **rl_kwargs)
+            if rate_limit_response:
+                # Rate limit exceeded, return the HTTP response
+                return rate_limit_response
+            # Rate limit not exceeded, execute the function
+            return function(request, *args, **kwargs)
 
         return wrap
 
